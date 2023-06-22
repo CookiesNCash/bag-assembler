ec0d4cbeea84d055d3f147baa32f2928baa8f3d6
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-06-22 11:33:25 +0300

    delete archive dir

--- archive/front/weatherApi.js
+++
@@ -1,9 +0,0 @@
-const getWeatherApi = async (link) => {
-  const result = await fetch(link);
-  const data = await result.json();
-  return [data.current.feelslike_c, data.current.condition.text];
-};
-
-export default getWeatherApi;
-
-// const temperature = data.current.temp_c;
