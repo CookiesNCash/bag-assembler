de0ab44c6a47c316e2452795b6131e0e61274744
teregiray <dantonengier@mail.ru>
2023-06-23 12:22:39 +0300

    rework alg func, add 2 classes

--- src/ClothingItem.js
+++ src/ClothingItem.js
@@ -1,30 +1,30 @@
 class ClothingItem {
   static addCoat() {
-    return { item: "Пальто", count: 1 };
+    return { item: 'Пальто', count: 1 };
   }
 
   static addSweater() {
-    return { item: "Свитер", count: 1 };
+    return { item: 'Свитер', count: 1 };
   }
 
   static addJacket() {
-    return { item: "Куртка", count: 1 };
+    return { item: 'Куртка', count: 1 };
   }
 
   static addShorts() {
-    return { item: "Шорты", count: 1 };
+    return { item: 'Шорты', count: 1 };
   }
 
   static addTShirt() {
-    return { item: "Футболка", count: 1 };
+    return { item: 'Футболка', count: 1 };
   }
 
   static addSweatshirt() {
-    return { item: "Кофта", count: 1 };
+    return { item: 'Кофта', count: 1 };
   }
 
   static addPants() {
-    return { item: "Штаны", count: 1 };
+    return { item: 'Штаны', count: 1 };
   }
 }
 
--- src/algorithm.js
+++ src/algorithm.js
@@ -1,5 +1,5 @@
-import Data from "./Data.js";
-import ClothingItem from "./ClothingItem.js";
+import Data from './Data.js';
+import ClothingItem from './ClothingItem.js';
 
 const calculateLuggage = async (data) => {
   const dataProcessor = new Data(data);
@@ -20,22 +20,22 @@ const calculateLuggage = async (data) => {
   const pantsNeeded = Math.ceil(currentDays / 5);
   const sweatShirtNeeded = Math.ceil(currentDays / 4);
 
-  for (let i = 0; i < tShirtsNeeded; i++) {
+  for (let i = 0; i < tShirtsNeeded; i += 1) {
     collectedItems.push(ClothingItem.addTShirt());
   }
 
-  for (let i = 0; i < sweatShirtNeeded; i++) {
+  for (let i = 0; i < sweatShirtNeeded; i += 1) {
     collectedItems.push(ClothingItem.addSweatshirt());
   }
 
-  for (let i = 0; i < pantsNeeded; i++) {
+  for (let i = 0; i < pantsNeeded; i += 1) {
     collectedItems.push(ClothingItem.addPants());
   }
 
   const luggage = collectedItems.reduce((acc, item) => {
     const existingItem = acc.find((collectedItem) => collectedItem.item === item.item);
     if (existingItem) {
-      existingItem.count++;
+      existingItem.count += 1;
     } else {
       acc.push(item);
     }
